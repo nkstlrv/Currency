09dbf10a71e87b55866b83a22804396d10e30cbf
nkstlrv <stlrvnk@gmail.com>
2023-06-18 13:47:59 +0300

    currency app initial

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'currency',
 ]
 
 MIDDLEWARE = [
