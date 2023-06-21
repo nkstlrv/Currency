e061c4a77b716a01c580b3cd84e5d3a8aa3c2f58
nkstlrv <stlrvnk@gmail.com>
2023-06-21 21:01:26 +0300

    remade project  again

--- app/currency/apps.py
+++ app/currency/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CurrencyAppConfig(AppConfig):
+class CurrencyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'currency_app'
+    name = 'currency'
