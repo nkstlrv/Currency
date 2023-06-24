df7ec4a4672f1b78cc5fe05b87a80548bc6c1fe1
nkstlrv <stlrvnk@gmail.com>
2023-06-24 18:32:52 +0300

    ContuctUs model created

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Rate(models.Model):
     
-    buy = models.DecimalField(max_digits=8, decimal_places=2)
-    sell = models.DecimalField(max_digits=8, decimal_places=2)
-    currency = models.CharField(max_length=3)
+    buy = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    sell = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    currency = models.CharField(max_length=3, unique=True, blank=False, null=False)
     source = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
 
 
 class ContuctUs(models.Model):
-    pass
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    email_from = models.EmailField(blank=False, null=False)
+    subject = models.CharField(max_length=255, blank=False, null=False)
+    message = models.TextField(max_length=2500)
 
 
