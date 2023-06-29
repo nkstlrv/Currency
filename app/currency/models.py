6879e5f9e03909469d7c4164e5e8e15167e7ce3c
nkstlrv <stlrvnk@gmail.com>
2023-06-29 17:40:54 +0300

    models: only positive buy/sell values validation

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # from django.contrib.auth.models import User
 
 
 class Rate(models.Model):
-    buy = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    sell = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    buy = models.DecimalField(max_digits=8, decimal_places=2, default=0, validators=[MinValueValidator(0)])
+    sell = models.DecimalField(max_digits=8, decimal_places=2, default=0, validators=[MinValueValidator(0)])
     currency = models.CharField(max_length=4, unique=True)
     source = models.CharField(max_length=255, blank=True)
     created = models.DateTimeField(auto_now_add=True)
