36af6ca62a50f3105b94d4afaedd34a6eff5cf2c
nkstlrv <stlrvnk@gmail.com>
2023-06-29 17:22:02 +0300

    altered tables

--- app/currency/models.py
+++ app/currency/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Rate(models.Model):
     buy = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     sell = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    currency = models.CharField(max_length=3, unique=True, blank=False, null=False)
-    source = models.CharField(max_length=255)
+    currency = models.CharField(max_length=4, unique=True)
+    source = models.CharField(max_length=255, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -16,8 +16,8 @@ class Rate(models.Model):
 
 class ContactUs(models.Model):
     # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    email_from = models.EmailField(blank=False, null=False)
-    subject = models.CharField(max_length=255, blank=False, null=False)
+    email_from = models.EmailField()
+    subject = models.CharField(max_length=255)
     message = models.CharField(max_length=2500)
 
     def __str__(self):
