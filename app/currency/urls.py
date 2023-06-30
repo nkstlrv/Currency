2c85b60b671a73c88673a1dfaadf97d1c09bcf2f
nkstlrv <stlrvnk@gmail.com>
2023-06-30 11:44:51 +0300

    navbar routing configured

--- app/currency/urls.py
+++ app/currency/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from currency import views
 
 urlpatterns = [
-    path('', views.home_view),
+    path('', views.home_view, name='home'),
     path('contacts/json/', views.contacts_json, name='contacts_json'),
+    path('contacts/table/', views.contacts_table, name='contacts_table'),
     path('rates/json/', views.rates_json, name='rates_json'),
     path('rates/table/', views.rates_table, name='rates_table'),
 ]
