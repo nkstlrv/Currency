5de05b8cea22bf7f3eb5ad22138b8e335de0f96a
nkstlrv <stlrvnk@gmail.com>
2023-07-13 17:53:30 +0300

    Rate model to admin panel added

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,3 +1,27 @@
-# from django.contrib import admin
+from django.contrib import admin
+from currency.models import Rates
 
-# Register your models here.
+
+@admin.register(Rates)
+class RateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'buy',
+        'sell',
+        'currency',
+        'source',
+        'created',
+    )
+    list_filter = (
+        'currency',
+        ('created')
+    )
+    search_fields = (
+        'buy',
+        'sell',
+        'source',
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
