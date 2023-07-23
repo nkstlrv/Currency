02966614d5b3f2d512d584b52b6090b9eb95ad2c
nkstlrv <stlrvnk@gmail.com>
2023-07-23 13:51:06 +0300

    profile edit view

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(),
+    path('profile/<int:pk>/', views.ProgileView.as_view(), name='profile'),
     ]
--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 
 
+class ProgileView(UpdateView):
+    model = get_user_model()
+    template_name = 'authentication/profile_view.html'
+    success_url = reverse_lazy('profile')
+    fields = (
+        'first_name',
+        'last_name',
+        'email',
+        'username'
+    )
