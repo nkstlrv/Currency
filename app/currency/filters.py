c3994c39688974a0629ea574f689bea30521a09b
nkstlrv <stlrvnk@gmail.com>
2023-08-15 22:16:43 +0300

    django-filter Rate model created

---
+++ app/currency/filters.py
@@ -0,0 +1,8 @@
+import django_filters
+from .models import Rate
+
+
+class RateFilter(django_filters.FilterSet):
+    class Meta:
+        model = Rate
+        fields = ("source", "currency", "buy", "sell")
