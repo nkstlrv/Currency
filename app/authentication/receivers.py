47e1c44931e8c91da6710a1f6857cd3315886e22
nkstlrv <stlrvnk@gmail.com>
2023-08-19 17:25:25 +0300

    Signals: only numeric phone number set

--- app/authentication/receivers.py
+++ app/authentication/receivers.py
@@ -14,3 +14,11 @@ def user_lower_email(instance, *args, **kwargs):
 def user_set_username(instance, *args, **kwargs):
     if not instance.pk and not instance.username:
         instance.username = str(uuid4())
+
+
+@receiver(pre_save, sender=User)
+def user_set_phone_number(instance, *args, **kwargs):
+    if instance.phone_number:
+        instance.phone_number = "".join(
+            [num for num in instance.phone_number if num in range(0, 10)]
+        )
