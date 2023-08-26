6e58dc09a7b73b1321ed61a29dd73ba23d82bec7
nkstlrv <stlrvnk@gmail.com>
2023-08-26 22:24:26 +0300

    auth api routing

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,6 +24,5 @@ urlpatterns = [
     path("auth/", include("django.contrib.auth.urls")),
     path("auth/", include("authentication.urls")),
     path("api/v1/currency/", include("currency.api.urls")),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/', include("authentication.api.urls")),
 ]
