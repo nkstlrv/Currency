1838716c4639b94c9227e2536240166e351e8a95
nkstlrv <stlrvnk@gmail.com>
2023-08-27 00:06:30 +0300

    Middleware LoggingViewSet created

--- app/currency/api/urls.py
+++ app/currency/api/urls.py
@@ -8,6 +8,7 @@ router = DefaultRouter(trailing_slash=False)
 router.register('rate/', views.RateViewSet, basename='rate')
 router.register('source/', views.SourceViewSet, basename='source')
 router.register('contact-us/', views.ContactUsViewSet, basename='contact-us')
+router.register('logging/', views.LoggingViewSet, basename='logging')
 
 
 urlpatterns = [] + router.urls
--- app/currency/api/views.py
+++ app/currency/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RateSerializer, SourceSerializer, ContactUsSerializer, LoggingSerializer
 from .paginators import MainPagination
-from .filters import RateFilter, SourceFilter, ContactUsFilter
+from .filters import RateFilter, SourceFilter, ContactUsFilter, LoggingFilter
 from rest_framework import filters as rest_framework_filters
 from django_filters import rest_framework as filters
 
@@ -48,3 +48,15 @@ class ContactUsViewSet(viewsets.ModelViewSet):
     )
     filterset_class = ContactUsFilter
     ordering_fields = ('subject', 'email_from')
+
+
+class LoggingViewSet(viewsets.ModelViewSet):
+    queryset = RequestResponseLog.objects.all().order_by('-id')
+    serializer_class = LoggingSerializer
+    pagination_class = MainPagination
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        rest_framework_filters.OrderingFilter,
+    )
+    filterset_class = LoggingFilter
+    ordering_fields = ('method', 'path', "time", "created")
