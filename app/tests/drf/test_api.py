9bd5e5ec889c793118187f05fac05413b80606ee
Nikita <stlrvnk@gmail.com>
2023-09-02 16:54:24 +0300

    pytest: api rates get/post no payload (not auth)

--- app/tests/drf/test_api.py
+++ app/tests/drf/test_api.py
@@ -1,14 +1,9 @@
-from rest_framework.test import APIClient
-
-
-def test_get_api_rates(api_client):
+def test_get_api_rates_not_auth(api_client):
     response = api_client.get("/api/currency/rate/")
-    assert response.status_code == 200
+    assert response.status_code == 401
     assert response.json()
 
 
-def test_post_empty_api_rates(api_client):
+def test_post_empty_api_rates_not_auth(api_client):
     response = api_client.post("/api/currency/rate/")
-    assert response.status_code == 400
-    # print(response.json())
-    assert response.json()["source"] == ["This field is required."]
+    assert response.status_code == 401
