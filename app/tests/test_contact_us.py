3f107a5357ce0b73ecf99a42c39edec7f417d7f7
Nikita <stlrvnk@gmail.com>
2023-09-02 16:14:52 +0300

    pytest: contact us post valid

--- app/tests/test_contact_us.py
+++ app/tests/test_contact_us.py
@@ -3,7 +3,12 @@ def test_get_contactus_list(client):
     assert response.status_code == 200
 
 
-def test_post_empty_form_contactus(client):
+def test_post_empty_form_contactus_status(client):
+    response = client.post("/contacts/create/")
+    assert response.status_code == 200
+
+
+def test_post_empty_form_contactus_errors(client):
     response = client.post("/contacts/create/")
     assert response.status_code == 200
     assert response.context_data["form"].errors == {
@@ -11,3 +16,36 @@ def test_post_empty_form_contactus(client):
         "subject": ["This field is required."],
         "message": ["This field is required."],
     }
+
+
+def test_post_invalid_email_contactus_status(client):
+    payload = {
+        "email_from": "INVALID EMAIL",
+        "subject": "Test",
+        "message": "Test",
+    }
+    response = client.post("/contacts/create/", data=payload)
+    assert response.status_code == 200
+
+
+def test_post_invalid_email_contactus_errors(client):
+    payload = {
+        "email_from": "INVALID EMAIL",
+        "subject": "Test",
+        "message": "Test",
+    }
+    response = client.post("/contacts/create/", data=payload)
+    # breakpoint()
+    assert response.context_data["form"].errors == {
+        "email_from": ["Enter a valid email address."],
+    }
+
+
+def test_post_valid_email_contactus(client):
+    payload = {
+        "email_from": "VALID_EMAIL@MAIL.COM",
+        "subject": "Test",
+        "message": "Test",
+    }
+    response = client.post("/contacts/create/", data=payload)
+    assert response.status_code == 302
