17182faa2b1a4f497629cb90483084e77444fe81
Nikita <stlrvnk@gmail.com>
2023-09-02 15:39:43 +0300

    pytest: main list views get

--- app/tests/tests_demo.py
+++ app/tests/tests_demo.py
@@ -1,5 +1,25 @@
-def test_index(client):
+def test_get_index(client):
     response = client.get("/")
     assert (
         response.status_code == 302
     )  # index page is not completed so currently redirecting to Rates List page
+
+
+def test_get_rate_list(client):
+    response = client.get("/rates/list/")
+    assert response.status_code == 200
+
+
+def test_get_contactus_list(client):
+    response = client.get("/contacts/list/")
+    assert response.status_code == 200
+
+
+def test_get_source_list(client):
+    response = client.get("/sources/list/")
+    assert response.status_code == 200
+
+
+def test_get_middleware_logs_list(client):
+    response = client.get("/requestresponcelogs/list/")
+    assert response.status_code == 200
