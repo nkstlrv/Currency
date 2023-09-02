8ed038fc2a9a210cb1d2a119c303cd3196ee49c4
Nikita <stlrvnk@gmail.com>
2023-09-02 15:11:07 +0300

    pytest.ini config

--- app/tests/tests_demo.py
+++ app/tests/tests_demo.py
@@ -1,11 +1,33 @@
+from typing import Any
+import requests
 import pytest
 
 
-def func(a: int):
+def func(a: Any):
     return a
 
 
-def test():
-    test_value = 2
-    result = func(test_value)
-    assert result == 2
+def test_func_valid_data():
+    test_value1 = 2
+    test_value2 = -2
+    test_value3 = "Hello"
+    test_value4 = True
+    test_value5 = [1, 2, 3]
+
+    result1 = func(test_value1)
+    result2 = func(test_value2)
+    result3 = func(test_value3)
+    result4 = func(test_value4)
+    result5 = func(test_value5)
+
+    assert result1 == 2
+    assert result2 == -2
+    assert result3 == "Hello"
+    assert result4 is True
+    assert result5 == [1, 2, 3]
+
+
+def test_google():
+    url = "https://google.com"
+    response = requests.get(url)
+    assert response.status_code == 200
