aeaf6274be88835e37a05ddb7c298a996586e103
Nikita <stlrvnk@gmail.com>
2023-09-02 15:27:42 +0300

    pytest settings + index pet initial test

---
+++ app/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from django.core.management import call_command
+
+
+@pytest.fixture(autouse=True, scope="function")
+def enable_db_access_for_all_tests(db):
+    """
+    give access to database for all tests
+    """
--- app/tests/tests_demo.py
+++ app/tests/tests_demo.py
@@ -1,33 +1,5 @@
-from typing import Any
-import requests
-import pytest
-
-
-def func(a: Any):
-    return a
-
-
-def test_func_valid_data():
-    test_value1 = 2
-    test_value2 = -2
-    test_value3 = "Hello"
-    test_value4 = True
-    test_value5 = [1, 2, 3]
-
-    result1 = func(test_value1)
-    result2 = func(test_value2)
-    result3 = func(test_value3)
-    result4 = func(test_value4)
-    result5 = func(test_value5)
-
-    assert result1 == 2
-    assert result2 == -2
-    assert result3 == "Hello"
-    assert result4 is True
-    assert result5 == [1, 2, 3]
-
-
-def test_google():
-    url = "https://google.com"
-    response = requests.get(url)
-    assert response.status_code == 200
+def test_index(client):
+    response = client.get("/")
+    assert (
+        response.status_code == 302
+    )  # index page is not completed so currently redirecting to Rates List page
