107acab164b3a45df540f6a5476f2de8ac1e7982
Nikita <stlrvnk@gmail.com>
2023-09-02 16:59:17 +0300

    pytest: conftest api_client_auth

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,7 @@
 # from django.core.management import call_command
 import pytest
+import os
+from dotenv import load_dotenv
 
 
 @pytest.fixture(autouse=True, scope="function")
@@ -14,4 +16,17 @@ def api_client():
     from rest_framework.test import APIClient
 
     client = APIClient()
+    yield client
+
+
+@pytest.fixture()
+def api_client_auth():
+    from rest_framework.test import APIClient
+
+    load_dotenv()
+
+    client = APIClient()
+    client.login(
+        username=os.getenv("TEST_USERNAME"), password=os.getenv("TEST_PASSWORD")
+    )
     yield client
