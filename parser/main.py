059dc4b5a5dac5cd077259f3c562e8815b49f99d
Nikita <stlrvnk@gmail.com>
2023-09-30 20:59:32 +0300

    basic page parsing

--- parser/main.py
+++ parser/main.py
@@ -1,10 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+BASE_URL = ("https://auto.ria.com/uk/search/"
+            "?indexName=auto,order_auto,newauto_search"
+            "&categories.main.id=1"
+            "&brand.id[0]=59"
+            "&country.import.usa.not=-1"
+            "&price.currency=1"
+            "&abroad.not=0"
+            "&custom.not=1"
+            "&page=0&size=100")
 
-def main():
-    ...
+
+def main(url: str) -> None:
+
+    response = requests.get(url=url)
+    response.raise_for_status()
+    html_page: str = response.text
+
+    soup = BeautifulSoup(html_page, "html.parser")
+
+    search_content = soup.find("div", {"id": "searchResults"})
+    all_content = search_content.find_all("section", {"class": "ticket-item"})
+
+    for item in all_content:
+        print(item)
 
 
 if __name__ == "__main__":
-    ...
+    main(BASE_URL)
